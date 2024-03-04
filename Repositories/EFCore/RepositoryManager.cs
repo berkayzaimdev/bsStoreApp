420e486e728bd179af555a7a658499101e609b74
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-04 12:27:33 +0300

    Added Lazy Loading for BookRepository to RepositoryManager

--- Repositories/EFCore/RepositoryManager.cs
+++ Repositories/EFCore/RepositoryManager.cs
@@ -10,16 +10,21 @@ namespace Repositories.EFCore
     public class RepositoryManager : IRepositoryManager
     {
         private readonly RepositoryContext _context;
+        private readonly Lazy<IBookRepository> _bookRepository;
 
         public RepositoryManager(RepositoryContext context)
         {
             _context = context;
+            _bookRepository = new Lazy<IBookRepository>(() => new BookRepository(_context));
         }
 
-        public IBookRepository Book => new BookRepository(_context);
+        // public IBookRepository Book => new BookRepository(_context);
         // IoC'ye sadece RepositoryManager'ın kaydını yapabilmek için böyle bir kullanıma gittik.
         // Dilersek tek tek her repository için constructor'da geçebilirdik, veya AutoFac kullanabilirdik
         // Bunu da farklı bir kullanım olarak görmüş olalım
+
+        public IBookRepository Book => _bookRepository.Value;
+        // Sadece kullanıldığı anda getirmek için Lazy Loading yaparak gereksiz kaynak kullanımını azalttık
 
         public void Save()
         {
