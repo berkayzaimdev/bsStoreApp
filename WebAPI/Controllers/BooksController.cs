45e8c31fc82b85cdcf7a50fec9d6a7314dca284d
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-04 11:57:23 +0300

    commit

---
+++ WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,116 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Repositories;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _context.Books.ToList();
+            return Ok(books);
+        }
+
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            var book = _context
+                .Books
+                .Where(b => b.Id.Equals(id))
+                .SingleOrDefault();
+
+            if (book is null)
+                return NotFound(); //404
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book is null)
+                    return BadRequest(); // 400 
+
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id,
+            [FromBody] Book book)
+        {
+            try 
+            {
+                // check book?
+                var entity = _context
+                    .Books
+                    .Where(b => b.Id.Equals(id))
+                    .SingleOrDefault();
+
+                if (entity is null)
+                    return NotFound(); // 404
+
+                // check id
+                if (id != book.Id)
+                    return BadRequest(); // 400
+
+                entity.Title = book.Title;
+                entity.Price = book.Price;
+
+                _context.SaveChanges();
+
+                return Ok(book);
+            }
+            catch(Exception exc)
+            {
+                throw new Exception(exc.Message);
+            } 
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteAllBooks([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var entity = _context
+                .Books
+                .Where(b => b.Id.Equals(id))
+                .SingleOrDefault();
+
+                if (entity is null)
+                    return NotFound(new
+                    {
+                        statusCode = 404,
+                        message = $"Book with id:{id} could not found."
+                    });  // 404
+
+                _context.Books.Remove(entity);
+                _context.SaveChanges();
+                return NoContent();
+            }
+            catch(Exception exc)
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ WebAPI/Repositories/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Repositories.Config;
+
+namespace WebAPI.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfig()); // Data seeding method
+        }
+    }
+}
