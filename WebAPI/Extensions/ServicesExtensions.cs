7a00b28a5426b5b18d676a5d63a858e8424d5cd0
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-04 12:33:10 +0300

    Added ServicesExtensions

---
+++ WebAPI/Extensions/ServicesExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.EFCore;
+
+namespace WebAPI.Extensions
+{
+    public static class ServicesExtensions
+    {
+        // Extension sınıfı ve metodu yazmak için gerekli olan parametreyi geçtik
+        // Hangi nesneye extension metot yazmak istiyorsak o sınıfı this ile işaretleriz.
+        // Bu nesne IServiceCollection olabilir, başka bir interface de olabilir.
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) => services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Repositories.EFCore;
+using WebAPI.Extensions;
 using WebAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +12,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
+builder.Services.ConfigureSqlContext(builder.Configuration);
+// Yazdıgımız extension metodu uyguladık. Parametre olarak konfigürasyonu geçtik
 
 var app = builder.Build();
 
