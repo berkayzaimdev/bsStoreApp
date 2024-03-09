bfcf3679415eec390aa01d608258661de54c860c
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-10 01:35:52 +0300

    Modified the IBookRepository and its concrete class for GetAllBooksAsync method

--- Repositories/EFCore/BookRepository.cs
+++ Repositories/EFCore/BookRepository.cs
@@ -16,11 +16,13 @@ namespace Repositories.EFCore
         {
 
         }
-        public async Task<IEnumerable<Book>> GetAllBooksAsync(BookParams bookParams, bool trackChanges) 
-            => await FindAll(trackChanges)
-            .Skip((bookParams.PageNumber-1)*bookParams.PageSize)
-            .Take(bookParams.PageSize)
+        public async Task<PagedList<Book>> GetAllBooksAsync(BookParams bookParams, bool trackChanges)
+        {
+            var books = await FindAll(trackChanges)
             .ToListAsync();
+
+            return PagedList<Book>.ToPagedList(books, bookParams.PageNumber, bookParams.PageSize);
+        }
         public async Task<Book> GetOneBookByIdAsync(int id, bool trackChanges) => await FindByCondition(b => b.Id.Equals(id), trackChanges).SingleOrDefaultAsync();
         public void CreateOneBook(Book book) => Create(book);
         public void UpdateOneBook(Book book) => Update(book);
