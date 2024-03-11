f7fe1f67e05ef0a0b19221198da4ba548cdb77b9
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-11 14:58:06 +0300

    Modified ServiceManager for make it include AuthenticationService

--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using Entities.Dtos;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Repositories.Contracts;
 using Services.Contracts;
 using System;
@@ -13,16 +16,23 @@ namespace Services
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IBookService> _bookService;
-        public ServiceManager(IRepositoryManager repositoryManager, 
-            ILoggerService logger, 
-            IMapper mapper, 
+        private readonly Lazy<IAuthenticationService> _authenticationService;
+        public ServiceManager(IRepositoryManager repositoryManager,
+            ILoggerService logger,
+            IMapper mapper,
+            IConfiguration configuration,
+            UserManager<User> userManager,
             IBookLinks bookLinks) 
         {
             _bookService = new Lazy<IBookService>(() 
                 => new BookManager(repositoryManager, logger, mapper, bookLinks));
+
+            _authenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationManager(logger, mapper, userManager, configuration));
         }
 
         public IBookService BookService => _bookService.Value;
+
+        public IAuthenticationService AuthenticationService => _authenticationService.Value;
         // Lazy Loading, Service katmanında da sağlandı.
     }
 }
