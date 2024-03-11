d98802464f62c210860b4d18420e03a8eaa72453
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-11 11:12:25 +0300

    Added ShapedEntity and Modified DataShaper for implement it

---
+++ Entities/Models/ShapedEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class ShapedEntity
+    {
+        public int Id { get; set; } // Her varlığın ID'si olduğu için
+        public Entity Entity { get; set; }
+
+        public ShapedEntity()
+        {
+            Entity = new Entity();
+        }
+    }
+}
